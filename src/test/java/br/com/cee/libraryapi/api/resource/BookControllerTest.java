e5c5c383c1cd8fb8d59fed10c0e2145845d76b2f
cintiaelisa@gmail.com <cintiaelisa@gmail.com>
2021-07-06 08:42:20 -0300

    Criando classes de testes para testar Rest Controllers.

---
+++ src/test/java/br/com/cee/libraryapi/api/resource/BookControllerTest.java
@@ -0,0 +1,36 @@
+package br.com.cee.libraryapi.api.resource;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    @DisplayName("Deve criar um livro com sucesso.")
+    public void createdBookTest() {
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar um erro de validação quando não houver dados suficientes para a criação do livro.")
+    public void createInvalidBookTest() {
+
+    }
+
+}
+
+
