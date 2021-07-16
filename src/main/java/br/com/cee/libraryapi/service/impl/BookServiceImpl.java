c3468498c1811117681d0342df964fe96fa76097
Cintia Elisa <cintiaelisa@gmail.com>
2021-07-16 17:04:06 -0300

    Implementação dos métodos delete e update em BookServiceImpl.java.

--- src/main/java/br/com/cee/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/br/com/cee/libraryapi/service/impl/BookServiceImpl.java
@@ -26,17 +26,22 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public Optional<Book> getById(Long id) {
-
         return this.repository.findById(id);
     }
 
     @Override
     public void delete(Book book) {
-
+        if(book == null || book.getId() == null) {
+            throw new IllegalArgumentException("Book id can't be null");
+        }
+        this.repository.delete(book);
     }
 
     @Override
     public Book update(Book book) {
-        return null;
+        if(book == null || book.getId() == null) {
+            throw new IllegalArgumentException("Book id can't be null");
+        }
+        return this.repository.save(book);
     }
 }
